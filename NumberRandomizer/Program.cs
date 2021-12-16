e43f660720067b14025c781bb0630b0076a70c13
Quicksoapy <73587977+Quicksoapy@users.noreply.github.com>
2021-12-16 21:19:57 +0100

    initial commit

---
+++ NumberRandomizer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Timers;
+
+namespace NumberRandomizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int inputNumber = Convert.ToInt32(input);
+            string input2 = Console.ReadLine();
+            int inputNumber2 = Convert.ToInt32(input2);
+
+            List<int> intList = new List<int>();
+            for (int i = inputNumber; i < inputNumber2 + 1; i++)
+            {
+                intList.Add(i);
+            }
+
+            int time = DateTime.Now.Millisecond;
+            int output = intList[(time * inputNumber2)/1000];
+            Console.WriteLine(output);
+        }
+    }
+}
